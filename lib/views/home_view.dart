72334d46889bd7e343f35d9ee933731016267375
Nikhil Shinde <parvatisaud774@gmail.com>
2025-08-03 16:46:46 +0530

    pushit

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,6 +1,7 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:firstkick/services/apis.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
@@ -16,24 +17,51 @@ class HomeView extends StatelessWidget {
 
         if(snapshot.hasData)
         {
-          return ListView.builder(
-            itemCount: snapshot.data!.data!.memes!.length,
-            itemBuilder: (context, index) {
+          return GridView.custom(
+           gridDelegate: SliverQuiltedGridDelegate(
+             crossAxisCount: 4,
+            mainAxisSpacing: 4,
+            crossAxisSpacing: 4,
+          repeatPattern: QuiltedGridRepeatPattern.inverted,
+           pattern: [
+            QuiltedGridTile(2, 2),
+            QuiltedGridTile(1, 1),
+            QuiltedGridTile(1, 1),
+           QuiltedGridTile(1, 2),
+           QuiltedGridTile(1, 1),
+           ],
+        ),
+          
+         childrenDelegate: SliverChildBuilderDelegate(
+         (context, index)  {
 
               final meme = snapshot.data!.data!.memes![index];
-              return ListTile(
-                leading:CachedNetworkImage(
+              return Card(
+                child:Column(children: [
+                 
+                CachedNetworkImage(
                   imageUrl:meme.url!,
-                  placeholder:(context,url)=>CircularProgressIndicator(),
-                  errorWidget:(context,uri,error)=>Icon(Icons.error),
+                  fit:BoxFit.cover,
+                  placeholder:(context,url)
+                  {
+                    return SizedBox.shrink
+                  (
+                   child: CircularProgressIndicator());
+                  },
+                  errorWidget:(context,uri,error)
+                  =>Icon(Icons.error),
 
                 ),
-                title:Text(meme.name!),
-                subtitle:Text(meme.url!),
+               
+                
+                ],
+                ),
               );
+         
             },
+         ));
 
-          );
+          
         }
         else{
           return Center(
